fc8a5f03a9dce7d736550dcfeeaa807971ba2ee6
Dragan Pepic <d.pepic@hotmail.com>
2021-09-29 18:21:49 +0200

    Add project files.

---
+++ Propertiji/Kursevi.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Propertiji
+{
+	/// <summary>
+	/// Interaction logic for Kursevi.xaml
+	/// </summary>
+	public partial class Kursevi : Window
+	{
+		private Kurs Kurs { get; set; } = new();
+		private ObservableCollection<Kurs> ListaKursevi;
+
+
+		public Kursevi(ObservableCollection<Kurs> kursevi)
+		{
+			InitializeComponent();
+			DataContext = Kurs;
+			ListaKursevi = kursevi;
+		}
+
+		private void Otkazi(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void Unos(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(Kurs.Naziv) ||
+				string.IsNullOrWhiteSpace(Kurs.Naziv))
+				MessageBox.Show("Unesi bre nesto");
+			else
+			{
+				/*for (int indeks = 0; indeks < ListaKursevi.Count; indeks++)
+				{
+					if (ListaKursevi[indeks].Naziv == Kurs.Naziv)
+					{
+						MessageBox.Show("Duplikat!");
+						return;
+					}
+				}
+
+				foreach(Kurs k in ListaKursevi)
+				{
+					if (k.Naziv == Kurs.Naziv)
+					{
+						MessageBox.Show("Duplikat!");
+						return;
+					}
+				}*/
+
+				if (ListaKursevi.Where(k => k.Naziv.ToLower() == Kurs.Naziv.ToLower()).Any())
+				{
+					MessageBox.Show("Duplikat!");
+					return;
+				}
+				ListaKursevi.Add(Kurs);
+
+				Close();
+			}
+		}
+	}
+}
---
+++ Propertiji/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Propertiji
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<Kurs> Kursevi { get; set; } = new();
+		public MainWindow()
+		{
+			InitializeComponent();
+			dg.ItemsSource = Kursevi;
+		}
+
+		private void Klik(object sender, RoutedEventArgs e)
+		{
+			Kursevi kurs = new(Kursevi);
+			kurs.Owner = this;
+			kurs.ShowDialog();
+			
+		}
+	}
+
+	public class Polaznik
+	{
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+		public string PrezimeIIme { get => $"{Prezime} {Ime}"; }
+	}
+
+	public class Kurs
+	{
+		public string Naziv { get; set; }
+	}
+}
